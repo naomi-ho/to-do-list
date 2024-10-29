f07aec84330fc900440386bb43c88be7e5a1e23d
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2024-10-29 00:04:08 -0400

    Include initialLoad()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './ui/style.css';
 import Task from './models/Task';
+import initialLoad from './ui/initial-load';
+
+initialLoad();
 
 const item1 = new Task(1, 'finish project', 'blah blah', '2024-10-30', 'home');
 
