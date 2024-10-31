2f3045605feed716ac95bed58662038baee8638f
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2024-10-31 15:43:34 -0400

    Add event listener to 'Add task' button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import './ui/style.css';
 import Task from './models/Task';
+import Project from './models/Project';
 import initialLoad from './ui/initial-load';
+import { createTask } from './ui/add-task';
 
 initialLoad();
 
-const item1 = new Task(1, 'finish project', 'blah blah', '2024-10-30', 'home');
+const project1 = new Project(0, 'inbox');
+console.log(project1);
 
-console.log(item1);
+const taskBtn = document.getElementById('task-button');
+
+taskBtn.addEventListener('click', (e) => {
+  const item1 = new Task(
+    1,
+    'finish project',
+    'blah blah',
+    '2024-10-30',
+    'home',
+  );
+  console.log(item1);
+  createTask();
+});
