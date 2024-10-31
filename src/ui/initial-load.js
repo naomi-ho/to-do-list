73762e09cf08a2ecc730f9a0f1762a3bd4600d05
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2024-10-31 15:46:09 -0400

    Remove onload

--- src/ui/initial-load.js
+++ src/ui/initial-load.js
@@ -2,8 +2,6 @@ import addInbox from './inbox.js';
 import addTask from './add-task.js';
 
 export default function initialLoad() {
-  window.onload = (e) => {
-    addInbox();
-    addTask();
-  };
+  addInbox();
+  addTask();
 }
