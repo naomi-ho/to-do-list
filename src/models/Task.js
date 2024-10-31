25097408c554216b7c58345577acde2532abf6b4
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2024-10-31 15:44:31 -0400

    Add project property

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,10 +1,11 @@
 export default class Task {
-  constructor(id, title, description, dueDate, priority) {
+  constructor(id, title, description, dueDate, priority, project) {
     this.id = id;
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.project = project;
     this.completed = false; // initialise as not completed
   }
 }
