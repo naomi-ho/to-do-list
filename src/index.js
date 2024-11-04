07399d9082146d67956b52a2d398ff4b0522e8ac
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2024-11-04 14:10:59 -0500

    Removed click event listener for 'Add task' button

--- src/index.js
+++ src/index.js
@@ -2,23 +2,9 @@ import './ui/style.css';
 import Task from './models/Task';
 import Project from './models/Project';
 import initialLoad from './ui/initial-load';
-import { createTask } from './ui/add-task';
+import addTask, { createTask } from './ui/add-task';
 
 initialLoad();
 
 const project1 = new Project(0, 'inbox');
 console.log(project1);
-
-const taskBtn = document.getElementById('task-button');
-
-taskBtn.addEventListener('click', (e) => {
-  const item1 = new Task(
-    1,
-    'finish project',
-    'blah blah',
-    '2024-10-30',
-    'home',
-  );
-  console.log(item1);
-  createTask();
-});
